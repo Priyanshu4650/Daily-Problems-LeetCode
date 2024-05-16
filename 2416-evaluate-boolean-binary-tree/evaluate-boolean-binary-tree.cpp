72dc30846039e5b756d9a872c877ac197bd9608d
Priyanshu <108177957+Priyanshu4650@users.noreply.github.com>
2024-05-16 14:42:52 +0530

    Time: 9 ms (66.76%) | Memory: 17.4 MB (64.84%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -10,42 +10,15 @@
  * };
  */
 class Solution {
-    void postOrder(TreeNode *root, vector<int>& post) {
-        if(root) {
-            postOrder(root->left, post);
-            postOrder(root->right, post);
-            post.push_back(root->val);
-        }
-    }
 public:
     bool evaluateTree(TreeNode* root) {
         if(!root) return false;
-        
-        vector<int> post;
-        postOrder(root, post);
+        if(root->val == 1) return true;
+        if(root->val == 0) return false;
 
-        stack<int> st;
-        int i = 0;
-        while(i < post.size()) {
-            if(post[i] == 0 || post[i] == 1)
-                st.push(post[i]);
-            else if(post[i] == 2) {
-                int a = st.top();
-                st.pop();
-                int b = st.top();
-                st.pop();
-                st.push(a | b);                
-            }
-            else {
-                int a = st.top();
-                st.pop();
-                int b = st.top();
-                st.pop();
-                st.push(a & b);
-            }
-            i += 1;
-        }
+        if(root->val == 2) return evaluateTree(root->left) | evaluateTree(root->right);
+        if(root->val == 3) return evaluateTree(root->left) & evaluateTree(root->right);
 
-        return st.top();
+        return true;
     }
 };
