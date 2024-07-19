ab0a79fd6a9cff716869b2bbd4cc3fc2027273db
Priyanshu <108177957+Priyanshu4650@users.noreply.github.com>
2024-07-18 20:57:16 -0400

    Time: 933 ms (18.81%) | Memory: 496.1 MB (11.25%) - LeetSync

---
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,69 @@
+class Solution {
+    void merge(vector<pair<int, int> >& v, vector<int>& ans, int l, int mid, int h) {
+    
+        vector<pair<int, int>> t; // temporary array for merging both halves
+        int i = l;
+        int j = mid + 1;
+    
+        while (i < mid + 1 && j <= h) {
+    
+            // v[i].first is greater than all
+            // the elements from j till h.
+            if (v[i].first > v[j].first) {
+                ans[v[i].second] += (h - j + 1);
+                t.push_back(v[i]);
+                i++;
+            }
+            else {
+                t.push_back(v[j]);
+                j++;
+            }
+        }
+    
+        // if any elements left in left array
+        while (i <= mid)
+            t.push_back(v[i++]);
+        // if any elements left in right array
+        while (j <= h)
+            t.push_back(v[j++]);
+        // putting elements back in main array in
+        // descending order
+        for (int k = 0, i = l; i <= h; i++, k++)
+            v[i] = t[k];
+    }
+    void mergesort(vector<pair<int, int> >& v, vector<int>& ans,
+               int i, int j)
+    {
+        if (i < j) {
+            int mid = (i + j) / 2;
+
+            // calling mergesort for left half
+            mergesort(v, ans, i, mid);
+
+            // calling mergesort for right half
+            mergesort(v, ans, mid + 1, j);
+
+            // merging both halves and generating answer
+            merge(v, ans, i, mid, j);
+        }
+    }
+
+public:
+    vector<int> countSmaller(vector<int>& arr) {
+        vector<pair<int, int> > v;
+        int n = arr.size();
+        // inserting elements and corresponding index
+        // as pair
+        for (int i = 0; i < n; i++)
+            v.push_back({ arr[i], i });
+    
+        // answer array for keeping count
+        // initialized by 0,
+        vector<int> ans(n, 0);
+    
+        // calling mergesort
+        mergesort(v, ans, 0, n - 1);
+    
+        return ans;
+    }
+};
