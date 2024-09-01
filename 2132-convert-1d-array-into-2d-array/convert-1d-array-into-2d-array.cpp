26acb863cca9be2f087b73848be258ec0122c5f7
Priyanshu <108177957+Priyanshu4650@users.noreply.github.com>
2024-09-01 06:43:59 +0530

    Time: 92 ms (21.17%) | Memory: 92.7 MB (37.03%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        if (original.size() != m * n) {
-            return {}; // Return an empty 2D array if dimensions are incompatible
+        vector<vector<int>> mat(m, vector<int>(n, 0));
+        if(original.size() != m * n) {
+            return {};
         }
 
-        std::vector<std::vector<int>> mat(m, std::vector<int>(n, 0));
-
-        for (int i = 0; i < original.size(); ++i) {
-            int row = i / n; // Calculate row index
-            int col = i % n; // Calculate column index
-            mat[row][col] = original[i];
+        for(int i=0;i<original.size();i++) {
+            mat[i/n][i%n] = original[i];
         }
 
         return mat;
