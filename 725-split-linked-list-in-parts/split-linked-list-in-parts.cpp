9c3ec0f1a32b39401eafc5e1f9c077b8c42b7e64
Priyanshu <108177957+Priyanshu4650@users.noreply.github.com>
2024-09-08 17:56:25 +0530

    Time: 5 ms (48.50%) | Memory: 13.9 MB (61.52%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
@@ -1,68 +1,53 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
-private:
-    void reverse(ListNode *&head) {
-        ListNode *prev = NULL;
-        ListNode *curr = head;
-        ListNode *nextn;
-
-        while(curr) {
-            nextn = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextn;
-        }
-
-        head = prev;
-    }
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
-        vector<ListNode *> res;
-        ListNode *ptr = head;
-        int n = 0;
+        vector<ListNode*> ans(k);
 
-        while(ptr) {
-            n += 1;
-            ptr = ptr->next;
+        // get total size of linked list
+        int size = 0;
+        ListNode* current = head;
+        while (current != nullptr) {
+            size++;
+            current = current->next;
         }
 
-        ptr = head;
-        if(k >= n) {
-            while(ptr) {
-                res.push_back(new ListNode(ptr->val));
-                ptr = ptr->next;
+        // minimum size for the k parts
+        int splitSize = size / k;
+
+        // Remaining nodes after splitting the k parts evenly.
+        // These will be distributed to the first (size % k) nodes
+        int numRemainingParts = size % k;
+
+        current = head;
+        ListNode* prev = current;
+        for (int i = 0; i < k; i++) {
+            // create the i-th part
+            ListNode* newPart = current;
+            // calculate size of i-th part
+            int currentSize = splitSize;
+            if (numRemainingParts > 0) {
+                numRemainingParts--;
+                currentSize++;
             }
-            // Fill remaining slots with NULL
-            res.resize(k, NULL);
-        }
-        else {
-            int groupsize = n / k;
-            int remainder = n % k;
 
-            for(int i = 0; i < k; ++i) {
-                ListNode *hnew = ptr;
-                int group_count = groupsize + (i < remainder ? 1 : 0);
-                for(int j = 1; j < group_count && ptr; ++j) {
-                    ptr = ptr->next;
+            // traverse to end of new part
+            int j = 0;
+            while (j < currentSize) {
+                prev = current;
+                if (current != nullptr) {
+                    current = current->next;
                 }
-                if(ptr) {
-                    ListNode *next_ptr = ptr->next;
-                    ptr->next = NULL;
-                    ptr = next_ptr;
-                }
-                res.push_back(hnew);
+                j++;
             }
+
+            // cut off the rest of linked list
+            if (prev != nullptr) {
+                prev->next = nullptr;
+            }
+
+            ans[i] = newPart;
         }
 
-        return res;
+        return ans;
     }
 };
