5271ee9bbdae10093f61ba18b0b0c361cd1456e8
Priyanshu <108177957+Priyanshu4650@users.noreply.github.com>
2025-11-08 07:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (80.63%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int minimumOneBitOperations(int n) {
-        if(n == 0) return 0;
-        int shift = floor(log2(n));
-        int x = (1 << (shift + 1)) - 1;
-
-        return x - minimumOneBitOperations(n^(1 << shift));
+        if (n == 0) {
+            return 0;
+        }
+        
+        int k = 0;
+        int curr = 1;
+        while (curr * 2 <= n) {
+            curr *= 2;
+            k++;
+        }
+        
+        return (1 << (k + 1)) - 1 - minimumOneBitOperations(n ^ curr);
     }
 };
