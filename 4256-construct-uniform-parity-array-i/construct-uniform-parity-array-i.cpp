2418f1bcd1e52a6550f26b6bc1c7ebf30187989f
Priyanshu <108177957+Priyanshu4650@users.noreply.github.com>
2026-03-26 20:46:15 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (39.42%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // int countOdd = 0;
+        // for(auto num: nums1) {
+        //     countOdd += num % 2 == 1;
+        // }
+        // int countEven = nums1.size() - countOdd;
+        return true;
+    }
+};
